5820626e6e05e15ef539045a1c0534e70ce351ee
Robert Watkins <robert@watkins.net>
2019-01-11 18:52:42 -0600

    updated rest service for put and post refactored get method for notification channels updated javascript to accommodate refactored api

--- client/client.js
+++ client/client.js
@@ -44,15 +44,32 @@ fetch('http://localhost:8888/agents')
   });
 }
 
+function get_notification_channel(id){
+fetch('http://localhost:8888/notification_channels/' + id)
+  .then(function(response) {
+      return response.json();
+  })
+  .then(function(myJson) {
+    display_notification_channel(myJson);
+  });
+}
 
 function display_notification_channels(json){
     console.log(json);
-    var notification_table = document.getElementById("notificationChannelTable");
-    var table_len=(notification_table.rows.length);
-    var channel_name = json.notification_channels[0].channel_name;
-    var channel_type = json.notification_channels[0].channel_type;
-    var configuration = json.notification_channels[0].configuration;
-    var row = notification_table.insertRow(table_len).outerHTML="<tr><td>"+channel_name+"<br>Type: "+channel_type+"</td><td>url: "+configuration+"</td></tr>";
+        for(var i = 0; i < (json.notification_channels.length); i++) {
+        var id = json.notification_channels[i];
+        get_notification_channel(id);
+    }
+ }
+
+ function display_notification_channel(json){
+   var notification_table = document.getElementById("notificationChannelTable");
+   var table_len=(notification_table.rows.length);
+   var row = json.notification_channels[0].channel_id;
+   var channel_name = json.notification_channels[0].channel_name;
+   var channel_type = json.notification_channels[0].channel_type;
+   var configuration = json.notification_channels[0].configuration;
+   var row = notification_table.insertRow(table_len).outerHTML="<tr><td>"+channel_name+"<br>Type: "+channel_type+"</td><td>url: "+configuration+"</td></tr>";
 }
 
 function get_notification_channels(){

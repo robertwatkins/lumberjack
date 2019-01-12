722e32f976c1f4560f8063b8e4fff0e6883253b8
Robert Watkins <robert@watkins.net>
2019-01-12 10:32:53 -0600

    fixed bad character in shell script

--- data/sample_data.sh
+++ data/sample_data.sh
@@ -4,6 +4,5 @@ curl -i -X POST -H "Content-Type:application/json" http://localhost:8888/notific
 
 
 curl http://localhost:8888/agents
-curl -i -X POST -H "Content-Type:application/json" http://localhost:8888/agents -d {"agent":[{"agent_id":1,"agent_name”:”Another Test Log Agent","agent_type":"Apache","log_path":"/var/log","notification_channel":"Dev Slack","running_status":"Not Running","skill_type":"URL n-gram","training_status":"Not Trained"}]}
-
+curl -i -X POST -H "Content-Type:application/json" http://localhost:8888/agents -d '{"agent":[{"agent_id":1,"agent_name":"Another Test Log Agent","agent_type":"Apache","log_path":"/var/log","notification_channel":"Dev Slack","running_status":"Not Running","skill_type":"URL n-gram","training_status":"Not Trained"}]}'
 
